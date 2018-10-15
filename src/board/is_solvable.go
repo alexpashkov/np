0c532f0f51519a87d22908e10e8d42709aa84a4e
Alexander Pashkov <alexpashkov123@gmail.com>
2018-10-15 17:28:04 +0300

    Fix Inversions.

--- src/board/is_solvable.go
+++ src/board/is_solvable.go
@@ -1,20 +1,29 @@
 package board
 
+// Counts number of inversions for a tile in position x, y
 func TileInversions(board Board, x, y int) (inversions int) {
-	examinedTile := board[y][x]
-	board.ForEach(func(currTile Tile, _, _ int) {
-		if examinedTile.Val() > currTile.Val() {
-			inversions++
+	examinedTile := board.Tile(x, y)
+	for currY := y; currY < len(board); currY++ {
+		for currX := x; currX < len(board); currX++ {
+			currTile := board.Tile(currX, currY)
+			if currTile.Val() != 0 &&
+				examinedTile.Val() > currTile.Val() {
+				inversions++
+			}
 		}
+	}
+	return
+}
+
+// Counts total number of inversions for the board
+func Inversions(board Board) (inversions int) {
+	board.ForEach(func(_ Tile, x, y int) {
+		inversions += TileInversions(board, x, y)
 	})
 	return
 }
 
-func BoardInversions(board Board) {
-
-}
-
 // Checks if a board is solvable
-func IsSolvable(board Board) bool {
-
-}
+//func IsSolvable(board Board) bool {
+//
+//}
---
+++ src/board/is_solvable_test.go
@@ -0,0 +1,85 @@
+package board
+
+import "testing"
+
+func TestTileInversions(t *testing.T) {
+	type testCase struct {
+		x, y, expected int
+	}
+	board := Board{
+		{1, 2, 3},
+		{4, 5, 6},
+		{7, 8, 0},
+	}
+	testCases := []testCase{
+		{
+			x: 0, y: 0, expected: 0,
+		},
+		{
+			x: 1, y: 0, expected: 0,
+		},
+		{
+			x: 2, y: 0, expected: 0,
+		},
+		{
+			x: 0, y: 1, expected: 0,
+		},
+		{
+			x: 1, y: 2, expected: 0,
+		},
+	}
+	for _, tc := range testCases {
+		got := TileInversions(board, tc.x, tc.y)
+		if got != tc.expected {
+			t.Error("Wrong number of inversions, got ", got, "expected: ", tc.expected)
+		}
+	}
+}
+
+func TestInversions(t *testing.T) {
+	type testCase struct {
+		board    Board
+		expected int
+	}
+
+	testCases := []testCase{
+		{
+			board: Board{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 0},
+			},
+			expected: 0,
+		},
+		{
+			board: Board{
+				{2, 1, 3},
+				{4, 5, 6},
+				{7, 8, 0},
+			},
+			expected: 1,
+		},
+		{
+			board: Board{
+				{2, 3, 1},
+				{4, 5, 6},
+				{7, 8, 0},
+			},
+			expected: 2,
+		},
+		{
+			board: Board{
+				{1, 4, 2},
+				{5, 6, 3},
+				{7, 8, 0},
+			},
+			expected: 4,
+		},
+	}
+	for _, tc := range testCases {
+		bi := Inversions(tc.board)
+		if bi != tc.expected {
+			t.Error("Wrong number of inversions, got ", bi, "expected: ", tc.expected)
+		}
+	}
+}
