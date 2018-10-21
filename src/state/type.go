aab3308deba78abfb6ebd25f3b1f841b401fdd1d
Eric Lyahovets <elyahove@e1r6p8.unit.ua>
2018-10-21 17:35:05 +0300

    Make state fields public.

--- src/state/type.go
+++ src/state/type.go
@@ -9,20 +9,20 @@ import (
 type Id string
 
 type State struct {
-	parent *State
-	puzzle puzzle.Puzzle
+	Parent *State
+	Puzzle puzzle.Puzzle
 }
 
 func (s State) Id() (id Id) {
-	s.puzzle.ForEach(func(t puzzle.Tile, _, _ int) {
+	s.Puzzle.ForEach(func(t puzzle.Tile, _, _ int) {
 		id += Id(strconv.Itoa(t.Val()))
 	})
 	return
 }
 
 func (s State) G() (g int) {
-	p := s.parent
-	for ; p != nil; p = p.parent {
+	p := s.Parent
+	for ; p != nil; p = p.Parent {
 		g++
 	}
 	return
@@ -30,7 +30,7 @@ func (s State) G() (g int) {
 
 // takes heuristics function as a parameter
 func (s State) H(f heuristics.Fn) int {
-	return f(s.puzzle)
+	return f(s.Puzzle)
 }
 
 // takes heuristics function as a parameter
