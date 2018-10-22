45d76b3aabb9f884627543219603cd7e1f5e3da8
Alexander Pashkov <alexpashkov123@gmail.com>
2018-10-22 22:41:26 +0300

    Add CoordsFinder.

--- src/puzzle/type.go
+++ src/puzzle/type.go
@@ -17,6 +17,28 @@ func (p Puzzle) Tile(x, y int) Tile {
 	return p[y][x]
 }
 
+func CoordsFinder(p Puzzle) func(t Tile) (x, y int) {
+	type Coords struct{ x, y int }
+	knownCoords := make(map[Tile]Coords)
+	cy, cx := 0, 0
+	return func(t Tile) (x, y int) {
+		coords, ok := knownCoords[t]
+		if ok {
+			return coords.x, coords.y
+		}
+		for ; cy < p.Size(); cy++ {
+			for ; cx < p.Size(); cx++ {
+				tile := p[cy][cx]
+				knownCoords[tile] = Coords{x: cx, y: cy}
+				if tile.Val() == t.Val() {
+					break
+				}
+			}
+		}
+		return cx, cy
+	}
+}
+
 func (p Puzzle) Coords(t Tile) (x, y int) {
 	p.ForEach(func(ct Tile, cx, cy int) (shellContinue bool) {
 		if ct.Val() == t.Val() {
