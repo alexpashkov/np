303c43098cb9bae1c1a9303e925db9bf0b0aad60
Alexander Pashkov <alexpashkov123@gmail.com>
2018-10-28 23:24:46 +0200

    Add loop initialization.

--- src/path/find.go
+++ src/path/find.go
@@ -1,15 +1,19 @@
 package path
 
 import (
+	"fmt"
 	"github.com/alexpashkov/npuzzle/src/heuristics"
 	"github.com/alexpashkov/npuzzle/src/priority-queue"
 	"github.com/alexpashkov/npuzzle/src/puzzle"
+	"github.com/alexpashkov/npuzzle/src/state"
 )
 
 func Find(from, to puzzle.Puzzle, f heuristics.Func) {
 	openSet := priority_queue.New(f)
-	openSet.Push(from)
-	for openSet.Peek() {
+	fromState := state.State{Parent: nil, Puzzle: from}
+	openSet.Push(&fromState)
 
+	for openSet.Len() != 0 && !puzzle.Equal(openSet.Peek().(*state.State).Puzzle, to) {
+		fmt.Println(openSet.Peek())
 	}
 }
